ef4ea07afdad39be1e4269327ea8e3eb1733ee0a
Flemming <fleeri91@gmail.com>
2023-02-05 21:16:28 +0100

    removed styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = ({ label }: ButtonProps) => {
-  return <button className='text-2xl text-red-400'>{label}</button>;
+  return <button>{label}</button>;
 };
 
 export default Button;
