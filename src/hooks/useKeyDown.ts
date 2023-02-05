dd9aaab393774dffe9399cc02b79610630d66f2d
Flemming <fleeri91@gmail.com>
2023-02-05 23:04:36 +0100

    keydown hook

---
+++ src/hooks/useKeyDown.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+interface UseKeyDownProps {
+  key: string;
+  callback: () => void;
+}
+
+const useKeyDown = ({ key, callback }: UseKeyDownProps) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === key) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [key, callback]);
+};
+
+export default useKeyDown;
