f8f15e9ca921d750442424183d2869604348d1f5
Flemming <fleeri91@gmail.com>
2023-02-06 23:51:22 +0100

    Fixed stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Button from "./Button";
 
 export default {
-  title: "WindWaves/Button",
+  title: "WindWaves/General/Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
@@ -12,33 +12,7 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const PrimaryButton = Template.bind({});
 
 PrimaryButton.args = {
-  children: "Primary",
+  label: "Primary",
   variant: "primary",
   disabled: false,
-};
-
-export const SecondaryButton = Template.bind({});
-
-SecondaryButton.args = {
-  children: "Secondary",
-  variant: "secondary",
-  disabled: false,
-};
-
-export const PrimaryButtonRounded = Template.bind({});
-
-PrimaryButtonRounded.args = {
-  children: "Primary Rounded",
-  variant: "primary",
-  disabled: false,
-  rounded: true,
-};
-
-export const SecondaryButtonRounded = Template.bind({});
-
-SecondaryButtonRounded.args = {
-  children: "Secondary Rounded",
-  variant: "secondary",
-  disabled: false,
-  rounded: true,
 };
