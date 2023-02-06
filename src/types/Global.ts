2ae6c4a8ce3bab3f0e0846d641a2bc5606534fa6
Flemming <fleeri91@gmail.com>
2023-02-06 23:48:49 +0100

    Size type

--- src/types/Global.ts
+++ src/types/Global.ts
@@ -1,1 +1,17 @@
 export type ButtonVariants = "primary" | "secondary";
+
+export type Size =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | "auto"
+  | "full";
