0be99c25b6956d775a3fd29a2cddc7de5bd1254f
Flemming <fleeri91@gmail.com>
2023-05-31 23:24:13 +0200

    component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,12 @@
+/** General */
 export { default as Button } from './Button'
+export { default as Typography } from './Typography'
+
+/** Layout */
+export { default as Divider } from './Divider'
+export { Grid, Cell } from './Grid'
+
+/** Navigation */
+/** Data Entry */
+/** Data Display */
+/** Feedback */
